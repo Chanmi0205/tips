677ea10bc92d1d2dc22cf6cddbaca6fb5b560cf0
tips <chanmi1155@naver.com>
2021-10-21 15:08:49 +0900

    해충박멸 오류 수정

--- app/src/main/java/com/example/tips_/worm.java
+++ app/src/main/java/com/example/tips_/worm.java
@@ -10,11 +10,31 @@ import android.widget.Button;
 public class worm extends AppCompatActivity {
 
     private Button worm_go;
+    private Button cockroach;
+    private Button drosophila;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worm);
+
+        drosophila = findViewById(R.id.drosophila);
+        drosophila.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(worm.this , drosophila.class);
+                startActivity(intent);
+            }
+        });
+
+        cockroach = findViewById(R.id.cockroach);
+        cockroach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(worm.this , cockroach.class);
+                startActivity(intent);
+            }
+        });
 
         worm_go = findViewById(R.id.worm_go);
         worm_go.setOnClickListener(new View.OnClickListener() {
